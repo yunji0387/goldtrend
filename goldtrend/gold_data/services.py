a54ab57fbdb497b3a3a61b3d42c67ff3c11bd96f
yunji0387 <yunji0387@gmail.com>
2024-12-08 13:54:59 +0800

    Rename project to asset_data and update settings for ASGI, WSGI, and manage.py

--- goldtrend/gold_data/services.py
+++ goldtrend/gold_data/services.py
@@ -11,3 +11,15 @@ def get_gold_price():
     except requests.exceptions.RequestException as e:
         print(f"Error fetching gold price data: {e}")
         return None
+
+def get_asset_price(asset):
+    url = f'https://api.gold-api.com/price/{asset}'
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching asset - {asset} price data: {e}")
+        return None
