6ac26314103c699de515dc6a9dc6d2f2c81b2b24
yunji0387 <yunji0387@gmail.com>
2024-12-13 16:25:31 +0800

    Add AssetPriceSerializer and AssetPriceView for asset price retrieval

--- asset_data/asset_price/serializers.py
+++ asset_data/asset_price/serializers.py
@@ -6,3 +6,10 @@ class GoldPriceSerializer(serializers.Serializer):
     price = serializers.DecimalField(max_digits=20, decimal_places=6)
     symbol = serializers.CharField(max_length=10)
     updatedAtReadable = serializers.CharField(max_length=100)
+
+class AssetPriceSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=50)
+    updatedAt = serializers.DateTimeField()
+    price = serializers.DecimalField(max_digits=20, decimal_places=6)
+    symbol = serializers.CharField(max_length=10)
+    updatedAtReadable = serializers.CharField(max_length=100)
--- asset_data/asset_price/urls.py
+++ asset_data/asset_price/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GoldPriceView
+from .views import GoldPriceView, AssetPriceView
 
 urlpatterns = [
     path('gold-price/', GoldPriceView.as_view(), name='gold_price'),
+    path('asset-price/<str:asset>/', AssetPriceView.as_view(), name='asset_price'),
 ]
